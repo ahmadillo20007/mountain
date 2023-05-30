d78e7f2c23d65705c9b97a3e3c284cef7c72a6dc
ahmadillo20007 <ahmadilloqubijonov74@gmail.com>
2023-05-30 15:33:22 +0500

    awqe

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,8 @@ function App() {
 
 <Routes>
   <Route path='/' element={<Login/>}>
-    <Route path='' element={<SignIn/>}/>
-    <Route path='signup' element={<SignUp/>}/>
+    <Route path='' element={<SignUp/>}/>
+    <Route path='signin' element={<SignIn/>}/>
   </Route>
 
   <Route path='/home' element={<Home/>}/>
